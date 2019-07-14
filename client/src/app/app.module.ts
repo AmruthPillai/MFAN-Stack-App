4b81f167bc5815bd9f954d25c4e6fe8ab67c7cec
Amruth Pillai <im.amruth@gmail.com>
2019-07-15 00:15:02 +0530

    create new Angular project

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ArticlesComponent } from './articles/articles.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ArticlesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
